3fa689e2bc915095748cc7cf0e07b6314fe13856
awang418 <awang418@gatech.edu>
2023-11-27 16:46:49 -0500

    sentiment visualize

--- streamlit.py
+++ streamlit.py
@@ -1,7 +1,31 @@
 import streamlit as st
 import requests
 from app.komprehend import getSentiment
+import matplotlib.pyplot as plt
+import numpy as np
 
+def plot_sentiment_chart(sentiment_result):
+    labels = ['Negative', 'Neutral', 'Positive']
+    percentages = [sentiment_result['sentiment']['negative'],
+                   sentiment_result['sentiment']['neutral'],
+                   sentiment_result['sentiment']['positive']]
+
+    fig, ax = plt.subplots()
+
+    # Use pastel colors
+    pastel_colors = ['#C40233', '#BDBABB', '#127852']
+    
+    # Create a horizontal bar chart with pastel colors
+    ax.barh(labels, percentages, color=pastel_colors, linewidth=0.25)
+    
+    # Display percentages on the bars
+    for i, v in enumerate(percentages):
+        ax.text(v + 0.01, i, f'{v*100:.1f}%', color='black', va='center', fontweight='bold')
+
+    ax.set_xlabel('Percentage')
+    ax.set_title('Sentiment Analysis')
+
+    st.pyplot(fig)
 
 st.title("Sentiment Analyzer")
 user_input = st.text_input("Enter a sentence: ")
@@ -11,7 +35,10 @@ if st.button("Analyze Sentiment"):
         if user_input:
             # Use the Komprehend API to analyze sentiment
             sentiment_result = getSentiment(user_input)
-            # Display the result
-            st.write(f"Sentiment: {sentiment_result}")
+            # # Display the result
+            # st.write(f"Sentiment: {sentiment_result}")
+            # Plot sentiment chart
+            plot_sentiment_chart(sentiment_result)
         else:
             st.warning("Please enter a sentence.")
+
